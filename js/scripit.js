9602301e34e14641ca1e47b821a8c3dacf4b1fec
Maycon Rocha <maycon_gr@hotmail.com>
2023-05-24 20:04:29 -0300

    Update contact

--- js/scripit.js
+++ js/scripit.js
@@ -35,7 +35,7 @@ $(document).ready(function () {
 
   // typing text animation script
   var typed = new Typed(".typing", {
-    strings: ["Estudante de desenvolvimento web,", "Front-end."],
+    strings: ["Estudante de desenvolvimento web,", "Desenvolvedor Front-end."],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
